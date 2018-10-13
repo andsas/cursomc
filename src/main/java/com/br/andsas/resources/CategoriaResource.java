ec7224213b37c79b2cf6c36bd384b3ed870eadb7
Andre Santos <m1andsas@gmail.com>
2018-10-13 00:12:41 -0300

    Testando a primeira classe de domínio

--- src/main/java/com/br/andsas/resources/CategoriaResource.java
+++ src/main/java/com/br/andsas/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package com.br.andsas.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.br.andsas.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
-	@GetMapping(value="/listar")
-	public String listar() {
-	return "O REST está funciondo!";
+	@GetMapping
+	public List<Categoria> listar() {
+		
+		List<Categoria> categorias = new ArrayList<>();
+		
+		categorias.add(new Categoria(1, "Informatica"));
+		categorias.add(new Categoria(2, "Eletrodomesticos"));
+		categorias.add(new Categoria(3, "Cama, mesa e banho"));
+		
+		return categorias;
 	}
 
 }
